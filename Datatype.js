6e15ef72f17bb00d57e4b2eaa3d669e05813844e
MariamTass <99330000+MariamTass@users.noreply.github.com>
2022-03-03 15:35:47 +0100

    my upload

---
+++ Datatype.js
@@ -0,0 +1,29 @@
+console.log(typeof "foodie") //string
+console.log(typeof 4) //numbers
+console.log(typeof undefined) //undefined
+console.log(typeof null) //null 
+
+let x;
+let y;
+let m;
+let z;
+
+let v= 2
+let t= 7
+let g= 8
+let k= 5
+
+console.log(v, t, g, k)
+
+let firstName= "Mariam";
+let lastName= "Dauda";
+let maritalStatus= "single";
+let country= "Nigeria";
+let age= 40
+
+console.log(firstName, lastName, maritalStatus, country, age)
+
+let myAge= 40
+let yourAge= 55
+
+console.log(myAge, yourAge)
